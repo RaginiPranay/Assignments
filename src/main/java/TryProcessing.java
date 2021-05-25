1537003e55f44af6c7b8aa35e56e51ab7c27a71f
Ragini Pranay <raginipranay@gmail.com>
2021-05-25 23:16:22 +0530

    4 balls question final -- procedural attempt

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -1,5 +1,12 @@
 import processing.core.PApplet;
 public class TryProcessing extends PApplet {
+    public static final int width=648;
+    public static final int height=488;
+    public static final int diameter=10;
+
+    public static int start[]={0,0,0,0};
+
+
     public static void main(String[] args) {
 
         PApplet.main("TryProcessing", args);
@@ -9,6 +16,25 @@ public class TryProcessing extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(648,488);
+        size(width,height);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+    @Override
+    public void draw()
+    {
+        for(int i=0;i<4;i++)
+        {
+            drawcustm(i);
+        }
+    }
+
+    private void drawcustm(int num) {
+
+        ellipse(start[num],(num+1)*height/5,diameter,diameter);
+        start[num]+=num+1;
     }
 }
